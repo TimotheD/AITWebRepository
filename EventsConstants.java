d1c9803ce8475b9645262394a2ae3b5a26849d46
Sylvain Edoukou <sedoukou@gmail.com>
2017-02-04 11:02:02 -0600

    Add files via upload

---
+++ EventsConstants.java
@@ -0,0 +1,40 @@
+package com.AitWeb.DataAccess;
+
+public class EventsConstants {
+
+	public static final String QUERY_SELECT = "SELECT";
+	public static final String QUERY_FROM =  "FROM";
+	public static final String QUERY_WHERE = "WHERE";
+	public static final String QUERY_AND = "AND";
+	public static final String QUERY_OR = "OR";
+	
+	public static final String QUERY_INSERT = "INSERT";
+	public static final String QUERY_INTO = "INTO";
+	public static final String QUERY_VALUES = "VALUES";
+	
+	public static final String QUERY_DELETE = "DELETE";
+	public static final String QUERY_UPDATE = "UPDATE";
+	public static final String QUERY_SET = "SET";
+	
+	public static final String QUERY_SELECT_ALL = "*";
+	public static final String QUERY_EQUAL = "=";
+	public static final String QUERY_NOT_EQUAL = "!=";
+	public static final String QUERY_START_PARENTHESIS = "(";
+	public static final String QUERY_END_PARENTHESIS = ")";
+	public static final String QUERY_LESS_THAN = "<";
+	public static final String QUERY_GREATER_THAN = ">";
+	public static final String QUERY_LESS_EQUALS_TO = "=<";
+	public static final String QUERY_GREATER_EQUALS_TO = ">=";
+	public static final String QUERY_COMMA = ",";
+	public static final String QUERY_SEMICOLON = ";";
+	
+	public static final String DB_USERNAME = "root";
+	public static final String DB_PASSWORD = "Ivoirien1";
+	public static final String DB_URL = "jdbc:mysql://localhost";
+	
+	
+	
+	
+	public static final String QUERY_DB_EVENT_TABLE_NAME = "aitnewwebsite";
+	
+}
