f76d3efebad8240fb606b8671e56204c36b681f7
timothe <timothe@192.168.0.4>
2017-02-04 01:29:59 -0600

    increase date

--- struts2-showcase/src/org/apache/struts2/showcase/DateAction.java
+++ struts2-showcase/src/org/apache/struts2/showcase/DateAction.java
@@ -1,31 +1,12 @@
-/*
- * $Id$
- *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-package org.apache.struts2.showcase;
 
-import com.opensymphony.xwork2.ActionSupport;
+package org.apache.struts2.showcase;
 
 import java.text.DateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+
+import com.opensymphony.xwork2.ActionSupport;
 
 /**
  * <code>DateAction</code>
@@ -86,7 +67,7 @@ public class DateAction extends ActionSupport {
 	public String browse() throws Exception {
 		Calendar cal = GregorianCalendar.getInstance();
 		now = cal.getTime();
-		cal.roll(Calendar.DATE, -1);
+		cal.roll(Calendar.DATE, -7);
 		cal.roll(Calendar.HOUR, -3);
 		past = cal.getTime();
 		cal.roll(Calendar.DATE, 2);
