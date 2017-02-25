17c1bc177e6ae86cafb2c71bccdd23323a1ed6a0
sylvain Edoukou <sedoukou@gmail.com>
2017-02-25 13:51:18 -0600

    Adding the POJOs and the DBconnection class with a few queries

---
+++ AitNewWebsite/src/com/AitWeb/jdbc/Util/dbException.java
@@ -0,0 +1,15 @@
+package com.AitWeb.jdbc.Util;
+
+public class dbException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7213961203354379343L;
+
+	
+	private void StaleConnection( Exception se) {
+		
+		
+	}
+}
